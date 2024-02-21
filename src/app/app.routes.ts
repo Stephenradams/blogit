cf9a6afbdb795a9028974beabe5433d061abdd9c
Stephen Adams <stephen.adams_co@realwear.com>
2024-02-21 16:23:39 +0000

    intial commit of project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { EditorComponent } from './editor/editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    component: PostsComponent,
+  },
+  {
+    path: 'posts/:id',
+    component: PostsComponent,
+  },
+  {
+    path: 'editor',
+    component: EditorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+];
